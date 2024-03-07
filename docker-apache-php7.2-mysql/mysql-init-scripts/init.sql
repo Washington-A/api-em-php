a3b6f2ab15f120a5c90cafced454a2b3d851e781
Washington-A <washingtonarau03@gmail.com>
2024-03-07 20:16:17 -0300

    inicialização de tabela automaticamente

---
+++ docker-apache-php7.2-mysql/mysql-init-scripts/init.sql
@@ -0,0 +1,8 @@
+USE devbuild;
+
+CREATE TABLE IF NOT EXISTS usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL
+);
+
+INSERT INTO usuarios (nome) VALUES ('Evandro Gomes'), ('Romilda Vane');
