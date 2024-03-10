d795267ab74a3e75830e5a7ef67c9596a5c981a3
Lucas <lucas1115guy@gmail.com>
2024-03-10 12:27:29 -0300

    Adicionado tratamento de erro e HTTP response em delete users/{id}

--- docker-apache-php7.2-mysql/www/api/users/delete.php
+++ docker-apache-php7.2-mysql/www/api/users/delete.php
@@ -1,17 +1,25 @@
 <?php
 
-if ($param == '') { echo json_encode(['ERRO' => "É necessário informar um id de user."]); exit; }
-
-if ($param < 1) { echo json_encode(['ERRO' => "É necessário informar um id de user."]); exit; }
+if ($param == '' || $param < 1) {
+    http_response_code(400);
+    echo json_encode(['erro' => "É necessário informar um id válido."]);
+    exit;
+}
 
 if ($param != '') {
+    try {
     $db = DB::connect();
     $rs = $db->prepare("DELETE FROM usuarios WHERE id={$param}");
     $exec = $rs->execute();
-
+    } catch (PDOException $e) {
+        http_response_code(500);
+        echo json_encode(["erro" => 'Erro: '. $e->getMessage()]);
+        exit;
+    }
     if ($exec && $rs->rowCount() > 0) {
-        echo json_encode(["dados" => 'Dados foram excluídos com sucesso.']);
-    } else {
-        echo json_encode(["dados" => 'Houve algum erro ao excluir os dados ou o ID não existe.']);
+        echo json_encode(["mensagem" => 'Dados foram excluídos com sucesso.']);
+    } else if ($rs->rowCount() === 0) {
+        http_response_code(404);
+        echo json_encode(["erro" => 'ID não existe.']);
     }
 }
